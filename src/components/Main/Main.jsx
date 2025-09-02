72aec48158e291820ce57ecebbfe398e16522fc3
Overton Dev Bell <overtonbellington@gmail.com>
2025-09-02 16:05:19 -0400

    feat: clothing items display dynamic

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,19 +1,29 @@
 import { WeatherCard } from "./WeatherCard";
 import { ItemCard } from "./ItemCard";
 import styles from "./Main.module.css";
+import { useWeatherContext } from "../../context/useWeatherApiContext";
+import { useTempContext } from "../../context/useTempContext";
 
 export function Main({ clothingItems, onCardClick }) {
+  const { weather, getTemperatureFromUnit } = useWeatherContext();
+  const { currentTemperatureUnit } = useTempContext();
+
+  const tempToDisplay = getTemperatureFromUnit(currentTemperatureUnit);
+
+  const itemsToDisplay = clothingItems.filter(
+    (item) => item.weather === weather
+  );
   return (
     <main className={styles.main}>
       <WeatherCard />
 
       <section className={styles.main__section}>
         <p className={styles.main__heading}>
-          <strong>Today is 75F / You may want to wear:</strong>
+          <strong>Today is {tempToDisplay} / You may want to wear:</strong>
         </p>
 
         <div className={styles.main__items}>
-          {clothingItems.map((card) => (
+          {itemsToDisplay.map((card) => (
             <ItemCard
               key={card._id}
               {...card}
