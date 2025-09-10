999483e017cbdc5d8ab34f76b1b89b33dba28af8
Overton Dev Bell <overtonbellington@gmail.com>
2025-09-10 10:17:04 -0400

    react router setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,26 @@ import App from "./components/App/App.jsx";
 import { TempProvider } from "./context/TempContext.jsx";
 import { LocationProvider } from "./context/LocationContext.jsx";
 import { WeatherProvider } from "./context/WeatherApiContext.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+let router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+  ],
+  {
+    basename: "/se_project_react",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <TempProvider>
       <LocationProvider>
         <WeatherProvider>
-          <App />
+          <RouterProvider router={router} />
         </WeatherProvider>
       </LocationProvider>
     </TempProvider>
