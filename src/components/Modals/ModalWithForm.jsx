fe2d69c8aff5b8d8facdad34e4f2aa00777c42f8
Overton Dev Bell <overtonbellington@gmail.com>
2025-09-15 15:55:30 -0400

    moved non jsx from components

--- src/components/Modals/ModalWithForm.jsx
+++ src/components/Modals/ModalWithForm.jsx
@@ -1,7 +1,7 @@
 import styles from "./ModalWithForm.module.css";
 import closeIcon from "../../assets/images/close.svg";
 import { useClothingItems } from "../../context/useClothingItems";
-import { useModalForm } from "./hooks/useModalForm";
+import { useModalForm } from "../../hooks/useModalForm";
 
 export function ModalWithForm({ title, name, buttonText, isOpen, onClose }) {
   const {
